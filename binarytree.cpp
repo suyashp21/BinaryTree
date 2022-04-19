a4a00d0f43ec9aa2275fdf0298b07ba0f8fdf43c
U-BSD\397142 <397142@SHS-10D9C06DD.beaverton.k12.or.us>
2022-04-19 10:34:57 -0700

    Second commit

--- binarytree.cpp
+++ binarytree.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 struct node {
   int data;
+  node* parent;
   node* left;
   node* right;
 };
 
 void add(node* head, node* n);
-bool search(node* head, int k);
+node* search(node* head, int k);
 
 int main() {
   node* head = NULL;
@@ -23,20 +24,84 @@ int main() {
       cout << "Enter the number to add: "; cin >> input;
       node* n = new node();
       n->data = input;
+      n->parent = NULL;
+      n->left = NULL;
+      n->right = NULL;
       if (head == NULL) {
         head = n;
       }
       else {
         add(head, n);
       }
+      // cout << head->data << endl;
     }
     else if (strcmp(action, "SEARCH") == 0 || strcmp(action, "search") == 0) {
       cout << "Enter the number to search for: "; cin >> input;
-      if (search(head, input)) {
+      if (search(head, input) != NULL) {
         cout << "Yes, that is in the tree." << endl;
       }
       else {
         cout << "No, that ain't in the tree." << endl;
+      }
+    }
+
+    else if (strcmp(action, "DELETE") == 0 || strcmp(action, "delete") == 0) {
+      cout << "Enter the number to delete: "; cin >> input;
+      node* n = search(head, input);
+      if (n == NULL) {
+	cout << "That number isn't in the tree!" << endl;
+      }
+      else {
+	if (n->left == NULL && n->right == NULL) { // no children
+	  if (head == n) {
+	    head = NULL;
+	    delete n;
+	  }
+	  else if (n->parent->left == n) {
+	    n->parent->left = NULL;
+	    delete n;
+	  }
+	  else if (n->parent->right == n) {
+	    n->parent->right = NULL;
+	    delete n;
+	  }
+	}
+	
+	else if (n->left == NULL) {
+	  if (head == n) {
+	    head = n->right;
+	    head->parent = NULL;
+	    delete n;
+	  }
+	  else if (n->parent->left == n) {
+	    n->parent->left = n->right;
+	    n->right->parent = n->parent;
+	  }
+	  else if (n->parent->right == n) {
+	    n->parent->right = n->right;
+	    n->right->parent = n->parent;
+	  }
+	}
+	
+	else if (n->right == NULL) {
+	  if (head == n) {
+	    head = n->left;
+	    head->parent = NULL;
+	    delete n;
+	  }
+          else if (n->parent->left == n) {
+            n->parent->left = n->left;
+            n->left->parent = n->parent;
+          }
+          else if (n->parent->right == n) {
+            n->parent->right = n->left;
+            n->left->parent = n->parent;
+          }
+
+	}
+	
+	else { // has both left and right
+	}
       }
     }
     else if (strcmp(action, "QUIT") == 0 || strcmp(action, "quit") == 0) {
@@ -52,6 +117,7 @@ void add(node* head, node* n) {
   if (head->data >= n->data) {
     if (head->left == NULL) {
       head->left = n;
+      n->parent = head;
     }
     else {
       add(head->left, n);
@@ -61,6 +127,7 @@ void add(node* head, node* n) {
   else if (head->data < n->data) {
     if (head->right == NULL) {
       head->right = n;
+      n->parent = head;
     }
     else {
       add(head->right, n);
@@ -68,13 +135,13 @@ void add(node* head, node* n) {
   }
 }
 
-bool search(node* head, int k) {
+node* search(node* head, int k) {
   if (head == NULL) {
-    return false;
+    return NULL;
   }
 
   else if (head->data == k) {
-    return true;
+    return head;
   }
 
   else if (head->data > k) {
